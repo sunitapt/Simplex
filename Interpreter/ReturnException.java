55e37e0022b3d15a6d9242713c31522f3004963a
Sunita Shantinath Patil <47025719+sunitapt@users.noreply.github.com>
2020-12-18 21:21:13 +0530

    Delete ReturnException.java

--- Interpreter/ReturnException.java
+++
@@ -1,9 +0,0 @@
-package com.github.sunitapt.Interpreter;
-
-public class ReturnException extends Exception{
-    int num;
-    public ReturnException(int num){
-        super(num+"");
-        this.num=num;
-    }
-}
